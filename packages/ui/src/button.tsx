0d0b08b2d22aab73adfffc13d68b2e38a06c4f48
Harshit Gautam <thegautamharshit@gmail.com>
2025-04-07 14:00:23 +0530

    Turborepo + Tailwindcss v4 + Prisma ORM

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
-}
-
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = () => {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
+    <button className="bg-amber-50 text-2xl border-2">Button</button>
   );
 };
